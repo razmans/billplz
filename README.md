a320f9da76eb15b896e234430370712227474c0e
razmans <razmans@gmail.com>
2024-11-09 14:07:21 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,145 @@
+# Billplz API Wrapper
+
+A TypeScript API wrapper for Billplz's billing and transaction API, enabling users to create, delete, and check bills and transactions.
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+  - [CreateBillplz](#createbillplz)
+  - [DeleteBillplz](#deletebillplz)
+  - [CheckBillplzStatus](#checkbillplzstatus)
+  - [GetTransactionListBillplz](#gettransactionlistbillplz)
+- [Interfaces](#interfaces)
+  - [CreateBillPayload](#createbillpayload)
+  - [BillDataPayload](#billdatapayload)
+- [License](#license)
+
+## Installation
+
+Install the required dependencies by adding them to your project.
+
+```bash
+npm install
+```
+
+## Usage
+
+### 1. CreateBillplz
+
+Creates a new bill in Billplz with the specified details.
+
+```typescript
+import { CreateBillplz, CreateBillPayload } from '@razmans/billplz';
+
+const billData: CreateBillPayload = {
+  id: 'your-billplz-id-xxxx-xxxx',
+  collection_id: 'your-collection-id',
+  description: 'Item description',
+  email: 'bob.abc@yopmail.com',
+  name: 'Bob Abc',
+  amount: 5000,
+  callback_url: 'https://yourwebsite.com/your-callback-url',
+  redirect_url: 'https://yourwebsite.com/redirect-url'
+};
+
+CreateBillplz(billData).then(response => {
+  console.log('Bill created:', response);
+}).catch(error => {
+  console.error('Error creating bill:', error);
+});
+```
+
+### 2. DeleteBillplz
+
+Deletes an existing bill in Billplz based on `billplzId`.
+
+```typescript
+import { DeleteBillplz, BillDataPayload } from '@razmans/billplz';
+
+const billData: BillDataPayload = {
+  id: 'your-billplz-id-xxxx-xxxx',
+  billplzId: 'your-collection-id',
+};
+
+DeleteBillplz(billData).then(response => {
+  console.log('Bill deleted:', response);
+}).catch(error => {
+  console.error('Error deleting bill:', error);
+});
+```
+
+### 3. CheckBillplzStatus
+
+Checks the status of a bill by `billplzId`.
+
+```typescript
+import { CheckBillplzStatus, BillDataPayload } from '@razmans/billplz';
+
+const billData: BillDataPayload = {
+  id: 'your-billplz-id-xxxx-xxxx',
+  billplzId: 'your-collection-id',
+};
+
+CheckBillplzStatus(billData).then(response => {
+  console.log('Bill status:', response);
+}).catch(error => {
+  console.error('Error checking bill status:', error);
+});
+```
+
+### 4. GetTransactionListBillplz
+
+Retrieves the list of transactions associated with a specific bill `billplzId`.
+
+```typescript
+import { GetTransactionListBillplz, BillDataPayload } from '@razmans/billplz';
+
+const billData: BillDataPayload = {
+  id: 'your-billplz-id-xxxx-xxxx',
+  billplzId: 'your-collection-id',
+};
+
+GetTransactionListBillplz(billData).then(response => {
+  console.log('Transaction list:', response);
+}).catch(error => {
+  console.error('Error retrieving transaction list:', error);
+});
+```
+
+## Interfaces
+
+### CreateBillPayload
+
+Defines the data structure for creating a bill.
+
+```typescript
+export interface CreateBillPayload {
+  id: string;
+  collection_id: string;
+  description: string;
+  email: string;
+  name: string;
+  amount: number;
+  callback_url: string;
+  redirect_url?: string;
+  reference_1_label?: string;
+  reference_1?: string;
+  reference_2_label?: string;
+  reference_2?: string;
+}
+```
+
+### BillDataPayload
+
+Defines the data structure for retrieving or deleting an existing bill.
+
+```typescript
+export interface BillDataPayload {
+  id: string;
+  billplzId: string;
+}
+```
+
+## License
+
+This project is licensed under the MIT License.
---
+++ index.ts
@@ -0,0 +1,179 @@
+/**
+ * @module CreateBillplz
+ * @param payload CreateBillPayload
+ * @returns
+ */
+/** Create billplz bill */
+
+export async function CreateBillplz(payload: CreateBillPayload): Promise<any> {
+  const username = payload.id;
+  const basicAuthToken = btoa(`${username}:`);
+
+  const mainPayload = { ...payload, id: '' };
+
+  try {
+    const response = await fetch(
+      `https://www.billplz-sandbox.com/api/v3/bills`,
+      {
+        method: 'POST',
+        body: JSON.stringify(mainPayload),
+        headers: {
+          Authorization: `Basic ${basicAuthToken}`,
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+
+    if (!response.ok) {
+      // If the response has an error status code, log the status and statusText for troubleshooting
+      throw new Error(`Error: ${response.status} ${response.statusText}`);
+    }
+
+    const responseData = await response.json();
+
+    return { response: responseData };
+  } catch (error) {
+    console.error('Error creating bill:', error);
+    throw error; // Re-throwing to allow further handling if necessary
+  }
+}
+
+/**
+ * @module DeleteBillplz
+ * @param payload BillDataPayload
+ * @returns
+ */
+/** Delete billplz bill */
+export async function DeleteBillplz(payload: BillDataPayload): Promise<any> {
+  const username = payload.id;
+  const basicAuthToken = btoa(`${username}:`);
+
+  try {
+    const response = await fetch(
+      `https://www.billplz-sandbox.com/api/v3/bills/${payload.billplzId}`,
+      {
+        method: 'DELETE',
+        headers: {
+          Authorization: `Basic ${basicAuthToken}`,
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+
+    if (!response.ok) {
+      // If the response has an error status code, log the status and statusText for troubleshooting
+      throw Error(`Error: ${response.status} ${response.statusText}`);
+    }
+
+    const responseData = await response.json();
+    // console.log('responseData', responseData);
+    return { response: responseData };
+  } catch (error) {
+    // console.error('Error creating bill:', error);
+    throw error; // Re-throwing to allow further handling if necessary
+  }
+}
+
+/**
+ * @module CheckBillplzStatus
+ * @param payload CheckBillStatusPayload
+ * @returns
+ */
+/** Check billplz bill status */
+export async function CheckBillplzStatus(
+  payload: BillDataPayload
+): Promise<any> {
+  const username = payload.id;
+  const basicAuthToken = btoa(`${username}:`);
+
+  try {
+    const response = await fetch(
+      `https://www.billplz-sandbox.com/api/v3/bills/${payload.billplzId}`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Basic ${basicAuthToken}`,
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+
+    if (!response.ok) {
+      // If the response has an error status code, log the status and statusText for troubleshooting
+      throw Error(`Error: ${response.status} ${response.statusText}`);
+    }
+
+    const responseData = await response.json();
+    // console.log('responseData', responseData);
+    return { response: responseData };
+  } catch (error) {
+    // console.error('Error creating bill:', error);
+    throw error; // Re-throwing to allow further handling if necessary
+  }
+}
+
+/**
+ * @module GetTransactionListBillplz
+ * @param payload BillDataPayload
+ * @returns
+ */
+/** Get all billplz transaction by ID */
+export async function GetTransactionListBillplz(
+  payload: BillDataPayload
+): Promise<any> {
+  const username = payload.id;
+  const basicAuthToken = btoa(`${username}:`);
+  // https://www.billplz.com/api/v3/bills/{BILL_ID}/transactions
+  try {
+    const response = await fetch(
+      `https://www.billplz.com/api/v3/bills/${payload.billplzId}/transactions`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Basic ${basicAuthToken}`,
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+
+    if (!response.ok) {
+      // If the response has an error status code, log the status and statusText for troubleshooting
+      throw Error(`Error: ${response.status} ${response.statusText}`);
+    }
+
+    const responseData = await response.json();
+    // console.log('responseData', responseData);
+    return { response: responseData };
+  } catch (error) {
+    // console.error('Error creating bill:', error);
+    throw error; // Re-throwing to allow further handling if necessary
+  }
+}
+
+/**
+ * @module CreateBillplzPayload
+ */
+/** Create billplz payload interface */
+export interface CreateBillPayload {
+  id: string;
+  collection_id: string;
+  description: string;
+  email: string;
+  name: string;
+  amount: number;
+  callback_url: string;
+  redirect_url?: string;
+  reference_1_label?: string;
+  reference_1?: string;
+  reference_2_label?: string;
+  reference_2?: string;
+}
+
+/**
+ * @module BillDataPayload
+ */
+/** Bill data billplz payload*/
+export interface BillDataPayload {
+  id: string;
+  billplzId: string;
+}
