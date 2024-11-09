33026eaa3f5a39edaf36a38ad395a6978acc8d46
razmans <razmans@gmail.com>
2024-11-09 15:58:40 +0800

    Add test functions for Billplz API operations and bump version to 0.1.9

--- index.ts
+++ index.ts
@@ -36,6 +36,48 @@ export async function CreateBillplz(payload: CreateBillPayload): Promise<any> {
 }
 
 /**
+ * @module CreateBillplzTest
+ * @param payload CreateBillPayload
+ * @returns
+ */
+/** Create billplz bill */
+
+export async function CreateBillplzTest(
+  payload: CreateBillPayload
+): Promise<any> {
+  const username = payload.id;
+  const basicAuthToken = btoa(`${username}:`);
+
+  const mainPayload = { ...payload, id: '' };
+
+  try {
+    const response = await fetch(
+      `https://www.billplz-sandbox.com/api/v3/bills`,
+      {
+        method: 'POST',
+        body: JSON.stringify(mainPayload),
+        headers: {
+          Authorization: `Basic ${basicAuthToken}`,
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+
+    if (!response.ok) {
+      // If the response has an error status code, log the status and statusText for troubleshooting
+      throw new Error(`Error: ${response.status} ${response.statusText}`);
+    }
+
+    const responseData = await response.json();
+
+    return { response: responseData };
+  } catch (error) {
+    console.error('Error creating bill:', error);
+    throw error; // Re-throwing to allow further handling if necessary
+  }
+}
+
+/**
  * @module DeleteBillplz
  * @param payload BillDataPayload
  * @returns
@@ -48,6 +90,44 @@ export async function DeleteBillplz(payload: BillDataPayload): Promise<any> {
   try {
     const response = await fetch(
       `https://www.billplz.com/api/v3/bills/${payload.billplzId}`,
+      {
+        method: 'DELETE',
+        headers: {
+          Authorization: `Basic ${basicAuthToken}`,
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+
+    if (!response.ok) {
+      // If the response has an error status code, log the status and statusText for troubleshooting
+      throw Error(`Error: ${response.status} ${response.statusText}`);
+    }
+
+    const responseData = await response.json();
+    // console.log('responseData', responseData);
+    return { response: responseData };
+  } catch (error) {
+    // console.error('Error creating bill:', error);
+    throw error; // Re-throwing to allow further handling if necessary
+  }
+}
+
+/**
+ * @module DeleteBillplzTest
+ * @param payload BillDataPayload
+ * @returns
+ */
+/** Delete billplz bill */
+export async function DeleteBillplzTest(
+  payload: BillDataPayload
+): Promise<any> {
+  const username = payload.id;
+  const basicAuthToken = btoa(`${username}:`);
+
+  try {
+    const response = await fetch(
+      `https://www.billplz-sandbox.com/api/v3/bills/${payload.billplzId}`,
       {
         method: 'DELETE',
         headers: {
@@ -110,6 +190,44 @@ export async function CheckBillplzStatus(
 }
 
 /**
+ * @module CheckBillplzStatusTest
+ * @param payload CheckBillStatusPayload
+ * @returns
+ */
+/** Check billplz bill status */
+export async function CheckBillplzStatusTest(
+  payload: BillDataPayload
+): Promise<any> {
+  const username = payload.id;
+  const basicAuthToken = btoa(`${username}:`);
+
+  try {
+    const response = await fetch(
+      `https://www.billplz-sandbox.com/api/v3/bills/${payload.billplzId}`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Basic ${basicAuthToken}`,
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+
+    if (!response.ok) {
+      // If the response has an error status code, log the status and statusText for troubleshooting
+      throw Error(`Error: ${response.status} ${response.statusText}`);
+    }
+
+    const responseData = await response.json();
+    // console.log('responseData', responseData);
+    return { response: responseData };
+  } catch (error) {
+    // console.error('Error creating bill:', error);
+    throw error; // Re-throwing to allow further handling if necessary
+  }
+}
+
+/**
  * @module GetTransactionListBillplz
  * @param payload BillDataPayload
  * @returns
@@ -123,6 +241,43 @@ export async function GetTransactionListBillplz(
   try {
     const response = await fetch(
       `https://www.billplz.com/api/v3/bills/${payload.billplzId}/transactions`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Basic ${basicAuthToken}`,
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+
+    if (!response.ok) {
+      // If the response has an error status code, log the status and statusText for troubleshooting
+      throw Error(`Error: ${response.status} ${response.statusText}`);
+    }
+
+    const responseData = await response.json();
+    // console.log('responseData', responseData);
+    return { response: responseData };
+  } catch (error) {
+    // console.error('Error creating bill:', error);
+    throw error; // Re-throwing to allow further handling if necessary
+  }
+}
+
+/**
+ * @module GetTransactionListBillplzTest
+ * @param payload BillDataPayload
+ * @returns
+ */
+/** Get all billplz transaction by ID */
+export async function GetTransactionListBillplzTest(
+  payload: BillDataPayload
+): Promise<any> {
+  const username = payload.id;
+  const basicAuthToken = btoa(`${username}:`);
+  try {
+    const response = await fetch(
+      `https://www.billplz-sandbox.com/api/v3/bills/${payload.billplzId}/transactions`,
       {
         method: 'GET',
         headers: {
